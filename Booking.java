561bd9e76961ceb5f46363459a00bee20fad8ea5
Luun0 <ekkozuparbaj@gmail.com>
2024-12-26 22:44:43 +0500

    Added Guest and Booking classes with basic structure

---
+++ Booking.java
@@ -0,0 +1,53 @@
+import java.time.LocalDate;
+
+public class Booking {
+    private Room room;
+    private Guest guest;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    public Booking(Room room, Guest guest, LocalDate checkInDate, LocalDate checkOutDate) {
+        this.room = room;
+        this.guest = guest;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{room=" + room + ", guest=" + guest +
+                ", checkInDate=" + checkInDate + ", checkOutDate=" + checkOutDate + "}";
+    }
+}
